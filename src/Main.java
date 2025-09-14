1f332bfff4e00e94bb8bf221cc928905261dc521
Engiavi <hackavi796@gmail.com>
2025-09-14 07:33:42 +0530

    write boilerplate for jdbc with necessary data fields and load the driver class

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Main{
+//    url for database connection with database name
+    private static final String url = "jdbc:mysql://localhost:3306/testmysql";
+    private static final String username = "root";
+    private static final String password = "qwerty";
+
+    public static void main(String [] args){
+//        below code is to load and register the driver class
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            System.out.println("Driver not found: " + e.getMessage());
+        }
+
+    }
+}
