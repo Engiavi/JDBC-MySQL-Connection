5a1eff9ce2ce8eb1c00125a61ba86c19eac5c30e
Engiavi <hackavi796@gmail.com>
2025-09-14 07:52:38 +0530

    Insertion in MySQL using java code

--- src/Main.java
+++ src/Main.java
@@ -17,17 +17,25 @@ public class Main{
         try{
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement stmt  = conn.createStatement();
-            String query = "SELECT * FROM students";
-            ResultSet rs    = stmt.executeQuery(query);
+//            String query = "SELECT * FROM students";
+            String query = String.format("INSERT INTO students (name, age, marks) VALUES ('%s', %o, %f)", "Kri",22,88.4);
+//            ResultSet rs    = stmt.executeQuery(query);
+
+            int rowsAffected = stmt.executeUpdate(query);
+            if(rowsAffected > 0){
+                System.out.println("Insertion successful, rows affected: " + rowsAffected);
+            } else {
+                System.out.println("Insertion failed, no rows affected.");
+            }
 
 //          now printing each row of table using while loop
-            while(rs.next()){
-               int id = rs.getInt("id");
-               String name = rs.getString("name");
-               int age = rs.getInt("age");
-               double marks = rs.getDouble("marks");
-                System.out.println("id : " + id + ", name : " + name + ", age : " + age + ", marks : " + marks);
-            }
+//            while(rs.next()){
+//               int id = rs.getInt("id");
+//               String name = rs.getString("name");
+//               int age = rs.getInt("age");
+//               double marks = rs.getDouble("marks");
+//                System.out.println("id : " + id + ", name : " + name + ", age : " + age + ", marks : " + marks);
+//            }
         }catch ( SQLException e){
             System.out.println("Connection failed: " + e.getMessage());
         }
